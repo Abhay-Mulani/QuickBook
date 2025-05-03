8f951b57f0236f0def690138c705381a13474283
Abhay Mulani <cayogeshmulani@gmail.com>
2025-05-03 17:03:29 +0530

    Set up Heroku deployment

--- Backend/server.js
+++ Backend/server.js
@@ -15,3 +15,15 @@ app.use('/api', bookingsRoute);
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
+
+const path = require('path');
+
+// Serve static files from the React app
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'frontend/build')));
+
+  // For all routes, serve the React app
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
